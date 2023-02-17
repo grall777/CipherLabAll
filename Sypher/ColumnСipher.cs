d79ffafea82bed7201d30dcf7986d36fbcfb145e
grall777 <vlad_shibut@mail.ru>
2023-02-17 17:54:31 +0300

    Добавьте файлы проекта.

---
+++ Sypher/ColumnСipher.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sypher
+{
+    class ColumnСipher
+    {
+        string key;
+        string text;
+        char[] charsKey;
+        char[] charsText;
+        
+
+        public void setKey(string key)
+        {
+            this.key = key;
+        }
+        public void splitKey()
+        {
+            charsKey = key.ToCharArray();
+        }
+        public void setText(string text)
+        {
+            this.text = text;
+        }
+        public void splitText()
+        {
+            charsText = text.ToCharArray();
+            
+        }
+        public void findSize()
+        {
+            int wight = charsKey.Length;
+            int high;
+            if (wight > charsText.Length)
+            {
+                high = 3;
+            }
+            else
+            {
+                high = charsKey.Length / wight + 2; // доделать
+                //Math.Ceiling();
+            }
+            char[,] table = new char[high, wight];
+
+            for (int i = 0; i < wight; i++)
+            {
+                table[0, i] = charsKey[i];
+            }
+
+            for (int i = 2; i < high; i++)
+            {
+                for (int j = 0; j < wight; j++)
+                {
+                    table[i, j] = charsText[j];
+                    Console.Write(table[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
---
+++ Sypher/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Sypher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColumnСipher cc = new ColumnСipher();
+
+            cc.setKey("Пиво");
+            cc.splitKey();
+            cc.setText("Выпивать");
+            cc.splitText();
+            cc.findSize();
+            
+        }
+    }
+}
