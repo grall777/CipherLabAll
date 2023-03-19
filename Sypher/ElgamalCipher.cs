5f66bf5a34a5dcf2b068eafd62b2830b1ec07b78
grall777 <vlad_shibut@mail.ru>
2023-03-19 15:42:36 +0300

    add Elgamal cipher

---
+++ Sypher/ElgamalCipher.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sypher
+{
+    public class ElgamalCipher
+    {
+        public void Encrypt()
+        {
+            int M = 5;
+            Console.WriteLine("Message: " + M);
+            Random rnd = new Random();
+            int value = rnd.Next(0, 12);
+            int[] simpleNumber = new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+                41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+
+            int p = simpleNumber[value];
+
+            int g = 7;
+
+
+
+            List<int> rand = new List<int>();
+            for (int i = 2; i < p - 1; i++)
+            {
+                rand.Add(i);
+            }
+            int x = rand[rnd.Next(0, rand.Count)];
+
+            double y = Math.Pow(g, x) % p;
+            int k = 0;
+            for (int i = 2; i < p - 1; i++)
+            {
+                if (find(i, p - 1) == 1)
+                {
+                    k = i;
+                    break;
+                }
+            }
+            double a = (Math.Pow(g, k)) % p;
+            double b = (Math.Pow(y, k)) * M % p;
+            double message = Decrypt(a, b, x, p);
+            Console.WriteLine("Шифротекст:(" + a + "," + b + ")");
+            Console.WriteLine("key: " + a + " " + b + " " + x + " " + p);
+        }
+
+        public double Decrypt(double a, double b, int x, int p)
+        {
+            double m = (b * (Math.Pow(a, p - 1 - x))) % p;
+            return m;
+        }
+
+        private int find(int a, int b)
+        {
+            if (a == 0)
+            {
+                return b;
+            }
+            else
+            {
+                while (b != 0)
+                {
+                    if (a > b)
+                    {
+                        a -= b;
+                    }
+                    else
+                    {
+                        b -= a;
+                    }
+                }
+                return a;
+            }
+
+        }
+    }
+}
+
--- Sypher/Program.cs
+++ Sypher/Program.cs
@@ -5,23 +5,29 @@ namespace Sypher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Столбцовый шифр");
-            ColumnСipher cc = new ColumnСipher();
-            cc.setKey("криптография");
-            cc.setText("перваялабораторнаяработапокиоки");
-            cc.findSize();
-            cc.findQueue();
-            cc.encrypt(); // бблрирркртптпяяквниеооааааоооаа
-            cc.decrypt("криптография", "бблрирркртптпяяквниеооааааоооаа");
+            //Console.WriteLine("Столбцовый шифр");
+            //ColumnСipher cc = new ColumnСipher();
+            //cc.setKey("криптография");
+            //cc.setText("перваялабораторнаяработапокиоки");
+            //cc.findSize();
+            //cc.findQueue();
+            //cc.encrypt(); // бблрирркртптпяяквниеооааааоооаа
+            //cc.decrypt("криптография", "бблрирркртптпяяквниеооааааоооаа");
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("Шифр Цезаря:");
+            //CaesarCipher ca = new CaesarCipher();
+            //string text = ca.Encrypt("абв", 1);
+            //Console.WriteLine(text);
+            //text = ca.Decrypt(text, 1);
+            //Console.WriteLine(text);
+
 
             Console.WriteLine();
-
-            Console.WriteLine("Шифр Цезаря:");
-            CaesarCipher ca = new CaesarCipher();
-            string text = ca.Encrypt("абв", 1);
-            Console.WriteLine(text);
-            text = ca.Decrypt(text, 1);
-            Console.WriteLine(text);
+            Console.WriteLine("Шифр Elgamal:");
+            ElgamalCipher el = new ElgamalCipher();
+            el.Encrypt();
 
         }
     }
